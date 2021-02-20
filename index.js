77aa10915397e633401435a0d1d80c8ed55782e3
ArchieRinn <brilliandar9428@gmail.com>
2021-02-20 13:41:21 +0900

    init

---
+++ index.js
@@ -0,0 +1,27 @@
+var express = require("express");
+var cors = require('cors');
+var bodyParser = require('body-parser')
+var app = express();
+
+app.use(cors());
+app.use(bodyParser.json())
+
+   app.get("/api/data", (req, res, next) => {
+    console.log("Connected");
+    res.status(200)
+   });
+
+   app.post("/api/data", (req, res, next) => {
+    console.log(req.body)
+    res.status(201)
+    res.json(req.body)
+   });
+
+   app.get("/", (req, res, next) => {
+     res.status(200)
+    res.json({status: true, message: "Connected"})
+   });
+
+app.listen(8000, () => {
+ console.log("Server running on port 8000");
+});
